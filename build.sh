893180c90f8102289025510080a0f2e19df52ddb
Vallabh Kansagara <vrkansagara@gmail.com>
2020-05-17 12:17:56 +0530

    adding instruction for project setup

--- build.sh
+++ build.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
-# git clone --depth=1 https://github.com/amix/vimrc.git -b vim ~/.vim
-# sh ~/.vim/build.sh
+# git clone --depth=1 https://github.com/vrkansagara/ide.git /tmp
+# sh /tmp/ide/build.sh
 
 # Rename .vim folder for backup
 mv ~/.vim ~/vim_old
 mv ~/.vimrc ~/.vimrc_old
 
-cp vimrc ~/.vimrc
+cp -Rf /tmp/ide ~/
+ln -s ~/vim/vimrc ~/.vimrc
