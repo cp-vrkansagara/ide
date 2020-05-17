bada16670ae5cb5b170f06fd487a2464cb58f63f
Vallabh Kansagara <vallabh@vrkansagara.local>
2020-05-17 12:08:17 +0530

    removal of unwanted files and directory

--- build.sh
+++ build.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
-git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime
-sh ~/.vim_runtime/install_awesome_vimrc.sh
-touch ~/.vim_runtime/my_configs.vim
-cp -f .vim/my_configs.vim  ~/.vim_runtime/
+
+# git clone --depth=1 https://github.com/amix/vimrc.git -b vim ~/.vim
+# sh ~/.vim/build.sh
+
+# Rename .vim folder for backup
+mv ~/.vim ~/vim_old
+mv ~/.vimrc ~/.vimrc_old
+
+cp vimrc ~/.vimrc
