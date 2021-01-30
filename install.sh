79ae102264750cee88aae7292c9476f31f4c5dd2
Vallabh Kansagara <vrkansagara@gmail.com>
2021-01-30 20:47:41 +0530

    improvement of installation script.
    Signed-off-by: Vallabh Kansagara <vrkansagara@gmail.com>

--- install.sh
+++ install.sh
@@ -1,21 +1,27 @@
 #!/usr/bin/env bash
+
 set -e
 
 CURRENT_DATE=$(date "+%Y%m%d%H%M%S")
 # This directory name must not start with .vim
 BACKUP_DIRECTORY_NAME="${HOME}/.old/vim-${CURRENT_DATE}"
 
+echo "Creating backup directory."
 if [ ! -d "$BACKUP_DIRECTORY_NAME" ]; then
   mkdir -p $BACKUP_DIRECTORY_NAME
 fi
 
+echo "Creating back of ~/.vim* to ${BACKUP_DIRECTORY_NAME}"
 if [ $(ls $HOME/.vim* | wc -l) != 0 ]; then
   mv -f $HOME/.vim*  $BACKUP_DIRECTORY_NAME
 fi
 
 #Clone main repository
+echo "Cloning the basic repository of [amix]"
 git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime
 sh ~/.vim_runtime/install_awesome_vimrc.sh
+
+echo "Cloning the [vrkansagar] vim configuration."
 git clone --recursive --branch master --depth 1 https://github.com/vrkansagara/ide.git /tmp/.vim-${CURRENT_DATE}
 cd /tmp/.vim-${CURRENT_DATE}
 git pull --recurse-submodules
@@ -26,7 +32,3 @@ cp $HOME/.vim/vimrc.vim $HOME/.vimrc
 cd $HOME
 
 echo "Installed the Ultimate Vim configuration of [vrkansagara] successfully! Enjoy :-)"
-# cp -f .vim/my_configs.vim  ~/.vim_runtime/
-
-
-
