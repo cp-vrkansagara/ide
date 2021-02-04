54ffe3dff470f197c85cbb3a07dbaeacfc9861be
Vallabh Kansagara <vallabh.kansagara@commercepundit.com>
2021-02-04 16:26:55 +0530

    adding random change for language vim and skeleton

--- skeleton/main.c
+++ skeleton/main.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 
 void main(){
-    printf("hello world !");
+	int i;
+	printf("hello world !");
 
-    for (int i = 0; i < 10;++i ) {
+    for (i= 0; i < 10;++i ) {
         printf("\n I is [%d]",i);
     }    
 }
