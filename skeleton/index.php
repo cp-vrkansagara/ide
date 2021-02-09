3a184d908d80698d791e32a15d90cc9143332f46
Vallabh Kansagara <vrkansagara@gmail.com>
2021-02-09 23:18:29 +0530

    conflicts resolved

--- skeleton/index.php
+++ skeleton/index.php
@@ -1,7 +1,15 @@
 <?php
+<<<<<<< HEAD
+=======
+
+>>>>>>> 2e00d4a (change of few things)
 define('PHP_START', microtime(true));
 
 /*error_reporting(E_ALL); ini_set('display_errors', '1'); ini_set("display_startup_errors", '1'); ini_set("log_errors", '1'); */ 
 
+<<<<<<< HEAD
 //,phpinfo();
+=======
+phpinfo();
+>>>>>>> 2e00d4a (change of few things)
 echo sprintf("Total execution time of script is [%2.5f] milisecounds",microtime(true) - PHP_START);
