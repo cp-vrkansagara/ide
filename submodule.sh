fe87aeea9703f5e661a97939aa66a1fabf373cf2
Vallabh Kansagara <vrkansagara@gmail.com>
2021-02-09 14:31:21 +0530

    vim-markdown

--- submodule.sh
+++ submodule.sh
@@ -13,6 +13,7 @@ git submodule add https://github.com/vim-airline/vim-airline.git bundle/vim-airl
 git submodule add https://github.com/ctrlpvim/ctrlp.vim.git bundle/ctrlp.vim
 git submodule add https://github.com/vim-scripts/bufexplorer.zip.git bundle/bufexplorer.zip
 git submodule add https://github.com/yegappan/mru.git bundle/mru.zip bundle/mru.zip
+git submodule add https://github.com/plasticboy/vim-markdown.git bundle/vim-markdown
 
 
 
