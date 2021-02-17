7a33415da23dc42a51de760aa751b1fcfda7e9f6
Vallabh Kansagara <vrkansagara@gmail.com>
2021-02-17 14:22:42 +0530

    Random commit at 20210217142242

--- update.sh
+++ update.sh
@@ -4,7 +4,7 @@ set -e
 CURRENT_DATE=$(date "+%Y%m%d%H%M%S")
 cd $HOME/.vim
 git add . 
-git commit -am"Random commit at ${CURRENT_DATEA}"
+git commit -am"Random commit at ${CURRENT_DATE}"
 
 git submodule update --init --recursive
 
