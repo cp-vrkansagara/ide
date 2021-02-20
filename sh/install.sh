73ebe87af443d9f199f14be25bd7abf6cac0f854
Vallabh Kansagara <vrkansagara@gmail.com>
2021-02-20 05:45:06 +0530

    Random commit at 20210220054506

--- sh/install.sh
+++ sh/install.sh
@@ -10,7 +10,13 @@ ${SUDO} apt-get install -y zsh guake ufw geany
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-${SUDO} apt-get install -y elinks htop lsof nmap git lsof ctags vim curl lsb-release net-tools
+echo "Installing network realated stuf "
+${SUDO} apt-get install -y iputils-ping net-tools lsof nmap 
+
+echo "System related stuff "
+${SUDO} apt-get install -y elinks htop git ctags vim curl lsb-release
+
+echo "Install desktop manager"
 ${SUDO} apt-get install -y xfce4 xfce4-goodies
 
 
