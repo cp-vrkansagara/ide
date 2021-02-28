9d95fbc628a9b9f78a2dca4db590aad5a827c1e4
Vallabh Kansagara <vrkansagara@gmail.com>
2021-02-28 14:38:46 +0530

    random

---
+++ sh/git/config.sh
@@ -0,0 +1,54 @@
+#!/usr/bin/env bash
+set -e
+
+CURRENT_DATE=$(date "+%Y%m%d%H%M%S")
+
+
+echo "Git configuration started on ${CURRENT_DATE}"
+# Git config list
+
+git config --global core.editor vim
+git config --global core.fileMode false
+
+
+git config --global diff.external "/usr/bin/meld"
+
+git config --global user.name "Vallabh Kansagara"
+git config --global help.autocorrect 0
+git config --global credential.help store
+
+git config --global alias.ls 'config --global -l'
+git config --global alias.ll 'log --oneline'
+
+git config --global alias.co checkout
+git config --global alias.br branch
+git config --global alias.ci commit
+git config --global alias.st status
+
+
+
+git config --global alias.last 'log -1 HEAD'
+git config --global alias.last 'log -1 HEAD --stat'
+
+git config --global alias.unstage 'reset HEAD --'
+
+
+git config --global alias.st 'status -sb'
+git config --global alias.visual '!gitk'
+git config --global alias.dv 'difftool -t vimdiff -y'
+
+
+git config --global alias.cm 'commit -m'
+git config --global alias.rv 'remote -v'
+
+
+
+# To count your commits is really easy and straightforward; here is the Git command:
+git config --global alias.meCommit 'rev-list --count'
+
+git config --global alias.gc 'gc --prune=now --aggressive'
+
+git config --global alias.work 'config --global user.email vallabh.kansagara@commercepundit.com'
+git config --global alias.personal 'config --global user.email vrkansagara@gmail.com'
+
+
