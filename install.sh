0aee0fac2cf5aebf24d062188874c20498b243ce
Vallabh Kansagara <vrkansagara@gmail.com>
2021-03-20 14:43:53 +0530

    improvement of installation script

--- install.sh
+++ install.sh
@@ -1,40 +1,44 @@
 #!/usr/bin/env bash
-set -e
+set -e # This setting is telling the script to exit on a command error.
+# set -x # You refer to a noisy script.(Used to debugging)
+
+if [ "$(whoami)" != "root" ]; then
+	SUDO=sudo
+fi
 
 CURRENT_DATE=$(date "+%Y%m%d%H%M%S")
 
 # This directory name must not start with .vim
-BACKUP_DIRECTORY_NAME="${HOME}/.old/vim-${CURRENT_DATE}"
+BACKUP_DIRECTORY="${HOME}/.old/vim-${CURRENT_DATE}"
+CLONE_DIRECTORY="/tmp/.vim-${CURRENT_DATE}"
 
 echo "Creating backup directory."
-if [ ! -d "$BACKUP_DIRECTORY_NAME" ]; then
-	mkdir -p $BACKUP_DIRECTORY_NAME
+if [ ! -d "$BACKUP_DIRECTORY" ]; then
+	mkdir -p $BACKUP_DIRECTORY
 fi
 
-echo "Creating back of ~/.vim* to ${BACKUP_DIRECTORY_NAME}"
+echo "Creating backup of ~/.vim* to ${BACKUP_DIRECTORY}"
 if [ $(ls $HOME/.vim* | wc -l) != 0 ]; then
 	echo "Moving base vimrc config to back up folder"
-	mv -f $HOME/.vim*  $BACKUP_DIRECTORY_NAME
+	mv -f $HOME/.vim*  $BACKUP_DIRECTORY
 fi
 
 echo "Cloning the [vrkansagar] vim configuration."
-git clone --recursive --branch master --depth 1 https://github.com/vrkansagara/ide.git /tmp/.vim-${CURRENT_DATE}
-cd /tmp/.vim-${CURRENT_DATE}
+git clone --recursive --branch master --depth 1 https://github.com/vrkansagara/ide.git ${CLONE_DIRECTORY}
+cd ${CLONE_DIRECTORY}
 
 # git pull --recurse-submodules
 git submodule update --init --recursive
-chmod +x bin/ -Rf
-mv /tmp/.vim-${CURRENT_DATE} $HOME/.vim
+mv ${CLONE_DIRECTORY} $HOME/.vim
 
 echo "Set up pathogen for vim run time path."
 mkdir -p ~/.vim/autoload ~/.vim/bundle && curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 
-# Copy of current vimrc to root directory cause to update .vim folder with git tracking so create symbolic link
 echo "Adding symbolic link for better git tracking of project"
 ln -s $HOME/.vim/vimrc.vim $HOME/.vimrc
 
-# Set sh directory executable
-chmod -R +x $HOME/.vim/src/sh/*
+# Set sh and bin  directory executable
+${SUDO} chmod -R +x $HOME/.vim/src/sh/* $HOME/.vim/bin
 
 # Before leaving the script reset to current working directory
 cd $HOME
